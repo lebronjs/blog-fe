04e18173b46a58d51cf2b0422bc7e52f0ed7d0c2
xuge <lebronjs@foxmail.com>
2024-03-13 17:37:17 +0800

    chore: we're gonna make it

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://lebronjs.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/blog-fe/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
