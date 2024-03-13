22a4f4093bc106d08cdac83ea9dc502972358dbf
xuge <lebronjs@foxmail.com>
2024-03-13 18:51:41 +0800

    fix: custom domain

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://lebronjs.github.io/',
+  url: 'https://blog.lebronjs.cn',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/blog-fe/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
